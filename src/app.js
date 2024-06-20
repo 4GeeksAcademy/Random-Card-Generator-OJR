447d72182b81b25b5003abcd71cc92b6fc905534
Oneil-R <167453370+Oneil-R@users.noreply.github.com>
2024-06-20 00:48:08 +0000

    complete

--- src/app.js
+++ src/app.js
@@ -1,11 +1,56 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateRandomCard();
 };
+
+const suits = ["hearts", "spades", "clubs", "diamonds"];
+const values = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "K",
+  "Q",
+  "J",
+  "A"
+];
+
+function generateRandomCard() {
+  const randomSuitIndex = Math.floor(Math.random() * suits.length);
+  const randomValueIndex = Math.floor(Math.random() * values.length);
+
+  const randomSuit = suits[randomSuitIndex];
+  const randomValue = values[randomValueIndex];
+
+  document.getElementById("cardIconTop").innerHTML = getSuitSymbol(randomSuit);
+  document.getElementById("cardIconBottom").innerHTML = getSuitSymbol(
+    randomSuit
+  );
+  document.getElementById("cardSuit").textContent = randomValue;
+
+  const card = document.getElementById("card");
+  card.className = `card ${randomSuit}`;
+}
+
+function getSuitSymbol(suit) {
+  switch (suit) {
+    case "hearts":
+      return '<i class="fas fa-heart"></i>';
+    case "spades":
+      return '<i class="fas fa-spade"></i>';
+    case "clubs":
+      return '<i class="fas fa-club"></i>';
+    case "diamonds":
+      return '<i class="fas fa-diamond"></i>';
+  }
+}
